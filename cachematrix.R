19fef49c5d53a49ccdcbf540203cd089164ed9bd
Bill Start <bill.start@monsanto.com>
2016-02-29 21:51:31 -0600

    Update cachematrix.R
    first edit

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,25 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inputClass <- class(x)
+  if(inputClass[1] != 'matrix')  {
+    stop('input must be a matrix, your input was a ', inputClass)
+  }
+#   m <- NULL
+#   set <- function(y) {
+#     x <<- y
+#     m <<- NULL
+#   }
+#   get <- function() x
+#   setmean <- function(solve) m <<- solve
+#   getmean <- function() m
+#   list(set = set, get = get,
+#        setmean = setmean,
+#        getmean = getmean)
+  cache <<- x
+  isInverted <<- FALSE
+  cache
+  #invisible()
 }
 
 
@@ -12,4 +30,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  if(isInverted == TRUE){
+    print('was already inverted')
+    return(invertedCache)
+    
+  }
+  isInverted <<- TRUE
+  print('inverted for the first time')
+  invertedCache <<- solve(cache)
+  invertedCache
 }
+
