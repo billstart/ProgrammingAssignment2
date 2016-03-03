a1514a55f1a4fa207609731db3f4c31be29d9380
Bill Start <bill.start@monsanto.com>
2016-03-02 22:15:39 -0600

    Update cachematrix.R
    clean up sample matrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,7 @@
 ## Write a short comment describing this function
 
 # sample starting matrix 
-# > mymatrix <- matrix(rnorm(1:16),4,4)
+# > mymatrix <- matrix(rnorm(16),4,4)
 
 makeCacheMatrix <- function(x = matrix()) {
   inputClass <- class(x)
